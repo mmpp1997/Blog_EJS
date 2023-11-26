1e364446aa6b638df4c321c0d03b3570364ad6d6
Mario Pedisic <mario.pedisic@gmail.com>
2023-11-26 18:51:01 +0100

    Complete index

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 var posts=[];
+var topics=["General","Drama","Sports","Movies","Other"]
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -14,19 +15,31 @@ app.get("/", (req, res) => {
 });
 
 app.get("/form", (req, res) => {
-  res.render("form.ejs");
+  res.render("form.ejs",{
+    topics:topics
+  });
+});
+app.get("/details", (req, res) => {
+  res.render("details.ejs");
 });
 
 app.post("/form", (req, res) => {
-  if(req.body["title"]!=""){
-  var post={title:req.body["title"],name:req.body["name"],email:req.body["email"],text:req.body["text"]};
-  posts.push(post)
+  var id=Math.floor(Math.random()*10000);
+  for(i=0;i<posts.length;i++){
+    if(id==posts[i].id){
+      var id=Math.floor(Math.random()*10000);
+      i=0;
+    }
+  }
+  var post={
+    id:id,
+    title:req.body["title"],
+    name:req.body["name"],
+    topic:req.body["topic"],
+    text:req.body["text"]};
+  posts.push(post);
   console.log(posts);
   res.redirect('/');
-  }
-  else{
-    console.log("nema naslova");
-  }
 });
 
 app.listen(port, () => {
