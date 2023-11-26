70131bc5d3ce4c9c2c1a349325e52b8b6a90b1bb
Mario Pedisic <mario.pedisic@gmail.com>
2023-11-26 20:43:32 +0100

    Complete details

--- index.js
+++ index.js
@@ -13,14 +13,22 @@ app.get("/", (req, res) => {
     data: posts
 });
 });
-
 app.get("/form", (req, res) => {
   res.render("form.ejs",{
     topics:topics
   });
 });
-app.get("/details", (req, res) => {
-  res.render("details.ejs");
+app.post("/details", (req, res) => {
+  var btn_id=req.body["detail"];
+  for(var i=0;i<posts.length;i++){
+    if(btn_id==posts[i].id){
+      var selected_post=posts[i];
+      break;
+    }
+  }
+  res.render("details.ejs",{
+    about:selected_post
+  });
 });
 
 app.post("/form", (req, res) => {
@@ -38,7 +46,6 @@ app.post("/form", (req, res) => {
     topic:req.body["topic"],
     text:req.body["text"]};
   posts.push(post);
-  console.log(posts);
   res.redirect('/');
 });
 
